1418d51f43c57d4912efb08f4bf8b8aee6af658f
Joonatan@work <joonatanrenel@gmail.com>
2018-09-24 18:09:22 +0300

    Got SPI to work, but this needs refactoring, fixes, review, etc....

--- spidrv.c
+++ spidrv.c
@@ -19,7 +19,7 @@
 /* SPI Configuration Parameter */
 const eUSCI_SPI_MasterConfig spiMasterConfig =
 {
-     EUSCI_B_SPI_CLOCKSOURCE_SMCLK, 12000000, 1000000, /* Currently desired SPI clock is 1 MHz */
+     EUSCI_B_SPI_CLOCKSOURCE_SMCLK, 12000000, 10000, /* Currently desired SPI clock is 1 MHz */
      EUSCI_B_SPI_MSB_FIRST,
      EUSCI_B_SPI_PHASE_DATA_CAPTURED_ONFIRST_CHANGED_ON_NEXT,
      EUSCI_B_SPI_CLOCKPOLARITY_INACTIVITY_HIGH, EUSCI_B_SPI_3PIN
@@ -121,6 +121,37 @@ Public void spidrv_cyclic50ms(void)
     if (++counter > 20u)
     {
         counter = 0u;
+
+
+        /* Setup the TX transfer characteristics & buffers */
+        MAP_DMA_setChannelControl(DMA_CH0_EUSCIB0TX0 | UDMA_PRI_SELECT,
+        UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE | UDMA_ARB_1);
+        MAP_DMA_setChannelTransfer(DMA_CH0_EUSCIB0TX0 | UDMA_PRI_SELECT,
+        UDMA_MODE_BASIC, priv_tx_data,
+                (void *) MAP_SPI_getTransmitBufferAddressForDMA(EUSCI_B0_BASE),
+                MAP_SPI_MSG_LENGTH);
+
+        /* Setup the RX transfer characteristics & buffers */
+        MAP_DMA_setChannelControl(DMA_CH1_EUSCIB0RX0 | UDMA_PRI_SELECT,
+        UDMA_SIZE_8 | UDMA_SRC_INC_NONE | UDMA_DST_INC_8 | UDMA_ARB_1);
+        MAP_DMA_setChannelTransfer(DMA_CH1_EUSCIB0RX0 | UDMA_PRI_SELECT,
+        UDMA_MODE_BASIC,
+                (void *) MAP_SPI_getReceiveBufferAddressForDMA(EUSCI_B0_BASE),
+                priv_rx_data,
+                MAP_SPI_MSG_LENGTH);
+
+
+        /* Enable DMA interrupt */
+        MAP_DMA_assignInterrupt(INT_DMA_INT1, 1);
+        MAP_DMA_clearInterruptFlag(DMA_CH1_EUSCIB0RX0 & 0x0F);
+
+        /* Assigning/Enabling Interrupts */
+        MAP_Interrupt_enableInterrupt(INT_DMA_INT1);
+        MAP_DMA_enableInterrupt(INT_DMA_INT1);
+
+
+
+
         /* Initial idea is that we test by sending/receiving a message every 1 second. */
         /* Start Rx */
         MAP_DMA_enableChannel(1);
