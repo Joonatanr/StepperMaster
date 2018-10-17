7da31e44c4f70172606263e38526c193effde56d
Joonatan@work <joonatanrenel@gmail.com>
2018-10-17 15:47:13 +0300

    Some general code cleanup for better readability.

--- spidrv.c
+++ spidrv.c
@@ -61,11 +61,11 @@ Private char priv_debug_str[64] = { 0 };
 Public void spidrv_init(void)
 {
     /* Configure CLK, MOSI & MISO for SPI0 (EUSCI_B0) */
-    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1,
-            GPIO_PIN5 | GPIO_PIN6, GPIO_PRIMARY_MODULE_FUNCTION);
-    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1,
-            GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1, GPIO_PIN5 | GPIO_PIN6, GPIO_PRIMARY_MODULE_FUNCTION);
+    MAP_GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN7, GPIO_PRIMARY_MODULE_FUNCTION);
 
+    /* Configure CS pin as output. */
+    /* TODO */
 
     /* Configuring SPI master module */
     MAP_SPI_initMaster(EUSCI_B0_BASE, &spiMasterConfig);
@@ -113,47 +113,47 @@ Private void startSpiCommunication(void)
     priv_tx_data[22] = priv_counter;
 
     /* Setup the TX transfer characteristics & buffers */
-        MAP_DMA_setChannelControl(DMA_CH0_EUSCIB0TX0 | UDMA_PRI_SELECT,
-        UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE | UDMA_ARB_1);
-        MAP_DMA_setChannelTransfer(DMA_CH0_EUSCIB0TX0 | UDMA_PRI_SELECT,
-        UDMA_MODE_BASIC, priv_tx_data,
-                (void *) MAP_SPI_getTransmitBufferAddressForDMA(EUSCI_B0_BASE),
-                MAP_SPI_MSG_LENGTH);
+    MAP_DMA_setChannelControl( DMA_CH0_EUSCIB0TX0 | UDMA_PRI_SELECT,
+                               UDMA_SIZE_8 | UDMA_SRC_INC_8 | UDMA_DST_INC_NONE | UDMA_ARB_1);
 
-        /* Setup the RX transfer characteristics & buffers */
-        MAP_DMA_setChannelControl(DMA_CH1_EUSCIB0RX0 | UDMA_PRI_SELECT,
-        UDMA_SIZE_8 | UDMA_SRC_INC_NONE | UDMA_DST_INC_8 | UDMA_ARB_1);
-        MAP_DMA_setChannelTransfer(DMA_CH1_EUSCIB0RX0 | UDMA_PRI_SELECT,
-        UDMA_MODE_BASIC,
-                (void *) MAP_SPI_getReceiveBufferAddressForDMA(EUSCI_B0_BASE),
-                priv_rx_data,
-                MAP_SPI_MSG_LENGTH);
+    MAP_DMA_setChannelTransfer(DMA_CH0_EUSCIB0TX0 | UDMA_PRI_SELECT,
+                               UDMA_MODE_BASIC,
+                               priv_tx_data,
+                               (void *) MAP_SPI_getTransmitBufferAddressForDMA(EUSCI_B0_BASE),
+                               MAP_SPI_MSG_LENGTH);
 
+    /* Setup the RX transfer characteristics & buffers */
+    MAP_DMA_setChannelControl( DMA_CH1_EUSCIB0RX0 | UDMA_PRI_SELECT,
+                               UDMA_SIZE_8 | UDMA_SRC_INC_NONE | UDMA_DST_INC_8 | UDMA_ARB_1);
 
-        /* TODO : Not sure if DMA interrupt assigning should be done here. */
+    MAP_DMA_setChannelTransfer(DMA_CH1_EUSCIB0RX0 | UDMA_PRI_SELECT,
+                               UDMA_MODE_BASIC,
+                               (void *) MAP_SPI_getReceiveBufferAddressForDMA(EUSCI_B0_BASE),
+                               priv_rx_data,
+                               MAP_SPI_MSG_LENGTH);
 
-        /* Enable DMA interrupt */
-        MAP_DMA_assignInterrupt(INT_DMA_INT1, 1);
-        MAP_DMA_clearInterruptFlag(DMA_CH1_EUSCIB0RX0 & 0x0F);
+    /* TODO : Not sure if DMA interrupt assigning should be done here. */
 
-        /* Assigning/Enabling Interrupts */
-        MAP_Interrupt_enableInterrupt(INT_DMA_INT1);
-        MAP_DMA_enableInterrupt(INT_DMA_INT1);
+    /* Enable DMA interrupt */
+    MAP_DMA_assignInterrupt(INT_DMA_INT1, 1);
+    MAP_DMA_clearInterruptFlag(DMA_CH1_EUSCIB0RX0 & 0x0F);
 
-        /* Initial idea is that we test by sending/receiving a message every 1 second. */
-        /* Start Rx */
-        MAP_DMA_enableChannel(1);
+    /* Assigning/Enabling Interrupts */
+    MAP_Interrupt_enableInterrupt(INT_DMA_INT1);
+    MAP_DMA_enableInterrupt(INT_DMA_INT1);
 
-        /* Start Tx */
-        MAP_DMA_enableChannel(0);
+    /* Initial idea is that we test by sending/receiving a message every 1 second. */
+    /* Start Rx */
+    MAP_DMA_enableChannel(1);
 
+    /* Start Tx */
+    MAP_DMA_enableChannel(0);
 
-        /* This is for debugging... */
-        if (++priv_counter > '9')
-        {
-            priv_counter = '0';
-        }
-
+    /* This is for debugging... */
+    if (++priv_counter > '9')
+    {
+        priv_counter = '0';
+    }
 }
 
 
